98703ea06b601f04d4f773024f18243acbdde84c
hangarita <hangarita@INV014.edistribution.loc>
2015-08-13 16:53:22 -0500

    Agregando recursos

--- public_html/index_edgeActions.js
+++ public_html/index_edgeActions.js
@@ -253,38 +253,7 @@
         Symbol.bindElementAction(compId, symbolName, "${btn_menugrafico}", "click", function (sym, e) {
             // muestra/oculta menu grafico
 
-            if (sym.$("menu_grafico").is(":visible")) {
-
-                sym.$("menu_grafico").hide();
-                sym.$("btn_full").hide();
-                sym.$("btn_creditos").hide();
-                sym.$("btn_ayuda").hide();
-                sym.$("btn_audio").hide();
-                sym.$("btn_info").hide();
-                sym.$("btn_accesibilidad").hide()
-                sym.$("Tool_accesibilidad").hide();
-                sym.$("Tool_info").hide();
-                sym.$("Tool_full").hide();
-                sym.$("Tool_ayuda").hide();
-                sym.$("Tool_audio").hide();
-                sym.$("Tool_creditos").hide();
-
-            } else {
-
-                sym.$("menu_grafico").show();
-                sym.$("btn_full").show();
-                sym.$("btn_creditos").show();
-                sym.$("btn_ayuda").show();
-                sym.$("btn_audio").show();
-                sym.$("btn_info").show();
-                sym.$("btn_accesibilidad").show();
-                sym.$("Tool_accesibilidad").show();
-                sym.$("Tool_info").show();
-                sym.$("Tool_full").show();
-                sym.$("Tool_ayuda").show();
-                sym.$("Tool_audio").show();
-                sym.$("Tool_creditos").show();
-            }
+            menu_tools_hide_show(sym);
 
 
         });
