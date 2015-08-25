c4ae240b84ff8a71bf89c01f622daaaaa052fce1
Heiner Angarita <heinerangarita@gmail.com>
2015-08-25 12:40:15 -0500

    Ended #1

--- public_html/compositions/elementos_plantilla/Accesibilidad/Accesibilidad_edgeActions.js
+++ public_html/compositions/elementos_plantilla/Accesibilidad/Accesibilidad_edgeActions.js
@@ -1,60 +1,61 @@
 /***********************
-* Acciones de composición de Adobe Edge Animate
-*
-* Editar este archivo con precaución, teniendo cuidado de conservar 
-* las firmas de función y los comentarios que comienzan con "Edge" para mantener la 
-* capacidad de interactuar con estas acciones en Adobe Edge Animate
-*
-***********************/
-(function($, Edge, compId){
-var Composition = Edge.Composition, Symbol = Edge.Symbol; // los alias más comunes para las clases de Edge
+ * Acciones de composición de Adobe Edge Animate
+ *
+ * Editar este archivo con precaución, teniendo cuidado de conservar 
+ * las firmas de función y los comentarios que comienzan con "Edge" para mantener la 
+ * capacidad de interactuar con estas acciones en Adobe Edge Animate
+ *
+ ***********************/
+(function ($, Edge, compId) {
+    var Composition = Edge.Composition, Symbol = Edge.Symbol; // los alias más comunes para las clases de Edge
 
-   //Edge symbol: 'stage'
-   (function(symbolName) {
-      
-      
-      
+    //Edge symbol: 'stage'
+    (function (symbolName) {
 
-      Symbol.bindElementAction(compId, symbolName, "${cerrar_acce}", "click", function(sym, e) {
-         parent.$(parent.document).trigger({
-                                 type: 'EDGE_Plantilla_ClosePopup',
-                                 sym: sym,
-                                 evt: e
-                             });
 
-      });
-      //Edge binding end
 
-      Symbol.bindElementAction(compId, symbolName, "${descargable2}", "click", function(sym, e) {
-         window.open("../../../media/pdf/accessibility.pdf", "_blank");
 
-      });
-      //Edge binding end
+        Symbol.bindElementAction(compId, symbolName, "${cerrar_acce}", "click", function (sym, e) {
+            parent.$(parent.document).trigger({
+                type: 'EDGE_Plantilla_ClosePopup',
+                sym: sym,
+                evt: e
+            });
 
-      Symbol.bindSymbolAction(compId, symbolName, "creationComplete", function(sym, e) {
-         // Insertar código para ejecutarse cuando el símbolo se crea aquí
-         $(".center-wrapper").css({overflow: "hidden"});
+        });
+        //Edge binding end
 
-      });
-      //Edge binding end
+        Symbol.bindElementAction(compId, symbolName, "${descargable2}", "click", function (sym, e) {
+            var stage = $(sym.getComposition().getStage().ele);
+            window.open(stage.prop("ed_identify").pdf, "_blank");
 
-   })("stage");
-   //Edge symbol end:'stage'
+        });
+        //Edge binding end
 
-   //=========================================================
-   
-   //Edge symbol: 'btn_cerrar_creditos'
-   (function(symbolName) {   
-   
-   })("btn_cerrar_creditos");
-   //Edge symbol end:'btn_cerrar_creditos'
+        Symbol.bindSymbolAction(compId, symbolName, "creationComplete", function (sym, e) {
+            // Insertar código para ejecutarse cuando el símbolo se crea aquí
+            $(".center-wrapper").css({overflow: "hidden"});
 
-   //=========================================================
-   
-   //Edge symbol: 'cerrar_acce'
-   (function(symbolName) {   
-   
-   })("cerrar_acce");
-   //Edge symbol end:'cerrar_acce'
+        });
+        //Edge binding end
+
+    })("stage");
+    //Edge symbol end:'stage'
+
+    //=========================================================
+
+    //Edge symbol: 'btn_cerrar_creditos'
+    (function (symbolName) {
+
+    })("btn_cerrar_creditos");
+    //Edge symbol end:'btn_cerrar_creditos'
+
+    //=========================================================
+
+    //Edge symbol: 'cerrar_acce'
+    (function (symbolName) {
+
+    })("cerrar_acce");
+    //Edge symbol end:'cerrar_acce'
 
 })(window.jQuery || AdobeEdge.$, AdobeEdge, "EDGE-13904317");
